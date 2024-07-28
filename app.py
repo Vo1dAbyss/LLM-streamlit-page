1a2ebae6274fe191cb901a5410afb9297ab5f349
Vo1dAbyss <juliozapatahernandez2006@gmail.com>
2024-07-28 14:12:09 +0100

    Init again

--- app.py
+++ app.py
@@ -1,62 +1,54 @@
+from hugchat import hugchat
+from hugchat.login import Login
 import streamlit as st
-import cohere
-import os
-import dotenv
 
-dotenv.load_dotenv()
+# Initialize session state variables if they don't exist
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+if "cookies" not in st.session_state:
+    st.session_state.cookies = None
 
-COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-
-client = cohere.Client(api_key=COHERE_API_KEY)
-
-st.set_page_config(page_title="LLM Chatbot")
-st.title("LLM Chatbot")
+st.title("HugChat LLM Chatbot")
 st.markdown("Made by Vo1d_s")
 
-def update_chat():
-    st.session_state.messages = []
-
 sidebar = st.sidebar
-#preamble_input = sidebar.text_input("Behaviour", placeholder="You are an assistant.", on_change=update_chat)
-temperature_slider = sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.5, step=0.05)
-max_tokens_slider = sidebar.slider("Max Tokens", min_value=10, max_value=1000, value=500, step=5)
-frequency_penalty_slider = sidebar.slider("Frequency penalty", min_value=0.0, max_value=1.0, value=0.75, step=0.05)
+sidebar.markdown("## HuggingFace account") 
+email = sidebar.text_input("Email", placeholder="Your email", disabled=st.session_state.logged_in)
+password = sidebar.text_input("Password (secure)", placeholder="Your password", type="password", disabled=st.session_state.logged_in)  # Hides the password input
 
-def chat(prompt):
-    response = ""
-    for event in client.chat_stream(
-        message=prompt,
-        #preamble=preamble_input or "You are an assistant.",
-        preamble="You are a helpfull assistant, you adapt to whatever the user says, so you can share likes and dislikes.",
-        chat_history=st.session_state.messages,
-        temperature=temperature_slider,
-        max_tokens=max_tokens_slider,
-        frequency_penalty=frequency_penalty_slider
-    ):
-        if event.event_type == "text-generation":
-            response = response + event.text
-            yield event.text
-            
-        if event.event_type == "stream-end":
-            return response
+if email and password:
+    if not st.session_state.logged_in:
+        try:
+            credentials = Login(email=email, passwd=password)
+            st.session_state.cookies = credentials.login()
+            st.session_state.logged_in = True
+            st.session_state.chatbot = hugchat.ChatBot(st.session_state.cookies)
+        except Exception as e:
+            sidebar.error(f"Error: {e}")
+
+def stream(prompt):
+    for event in st.session_state.chatbot.chat(prompt):
+        if event:
+            yield event["token"]
+
 def main():
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
     for message in st.session_state.messages:
-        with st.chat_message("assistant" if message["role"] == "CHATBOT" else "user"):
-            st.write(message["message"])
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("What is up?", disabled=not st.session_state.logged_in):
         with st.chat_message("user"):
             st.write(prompt)
-        
-        st.session_state.messages.append({"role": "USER", "message": prompt})
+
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
         with st.chat_message("assistant"):
-            response = st.write_stream(chat(prompt))
+            response = st.write_stream(stream(prompt))
 
-        st.session_state.messages.append({"role": "CHATBOT", "message": response})
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
     main()
